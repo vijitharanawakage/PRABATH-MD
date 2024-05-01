7c5f68255d45c41a854c5ace4fe9213a32799b5d
vijitharanawakage <168432129+vijitharanawakage@users.noreply.github.com>
2024-05-01 11:58:29 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,16 +1,12 @@
-/**
-░█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀█ ─█▀▀█ ▀▀█▀▀ ░█─░█ ── ░█▀▄▀█ ░█▀▀▄ 
-░█▄▄█ ░█▄▄▀ ░█▄▄█ ░█▀▀▄ ░█▄▄█ ─░█── ░█▀▀█ ▀▀ ░█░█░█ ░█─░█ 
-░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
+/**爪 尺 ㄩ几Ҝ几ㄖ山几 ' 丂 乃ㄖㄒ
 
-
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name SANDESH-MD
+* @authors Sandesh Bhashana & ddf-team <https://github.com/mrunknown>
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
 * @link <https://github.com/prabathLK/PRABATH-MD>
 * @version 2.0.6
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2023 sandesh bhashana, Sandesh-BOT. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
